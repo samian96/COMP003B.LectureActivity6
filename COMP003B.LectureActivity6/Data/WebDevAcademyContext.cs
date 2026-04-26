0d67a29dd3f8778a561b7e6632f9323820a07503
samian <sam.iannazzo@yahoo.com>
2026-04-26 11:20:45 -0700

    creasted data folder with WebDevContext

---
+++ COMP003B.LectureActivity6/Data/WebDevAcademyContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace COMP003B.LectureActivity6.Data
+{
+    public class WebDevAcademyContext : DbContext
+    {
+        public WebDevAcademyContext(DbContextOptions<WebDevAcademyContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+    }
+}
