edb979575a1b7d20bea6002ac2823c2b9bbedcf3
samian <sam.iannazzo@yahoo.com>
2026-04-26 11:06:07 -0700

    created course model

---
+++ COMP003B.LectureActivity6/Models/Course.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace COMP003B.LectureActivity6.Models
+{
+    public class Course
+    {
+        public int CourseId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        // Collection navigation property
+
+        public virtual ICollection<Enrollment>? Enrollments { get; set; }
+    }
+}
